557bd666bc784e312798016378cbc9f7c84542a1
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-04-10 16:53:45 +0300

    E

---
+++ tasks/contest_1/E/main.cpp
@@ -0,0 +1,77 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#include <string>
+#include <stack>
+#include <cassert>
+#include <iomanip>
+#include <random>
+#include <chrono>
+#include <cstring>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector <int>;
+using vvi = vector <vi>;
+using vll = vector <ll>;
+using pii = pair <int, int>;
+using vpii = vector <pii>;
+const double eps = 0.0001;
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD = 1e9 + 7;
+
+#define all(_x) _x.begin(), _x.end()
+#define rall(_x) _x.rbegin(), _x.rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(_x) (int)_x.size()
+#define display(_x) for (auto el : _x) cout << el << ' '; cout << '\n'
+#define cin_arr(_x) for (auto &el : _x) cin >> el;
+#define fast_input ios_base::sync_with_stdio(0)
+#define setpr cout << setprecision(6) << fixed
+
+const int MAXW = 10001;
+const int MAXN = 100;
+int n, W;
+int w[MAXN], c[MAXN];
+int dp[MAXW];
+
+void solve() {
+    cin >> n >> W;
+    for (int i = 0; i < n; i++) {
+        cin >> w[i];
+    }
+    for (int i = 0; i < n; i++) {
+        cin >> c[i];
+    }
+
+    fill(dp, dp + MAXW, -INF);
+    dp[0] = 0;
+    for (int i = 0; i < n; i++) {
+        for (int j = W; j >= w[i]; j--) {
+            dp[j] = max(dp[j], dp[j - w[i]] + c[i]);
+        }
+    }
+
+    cout << *max_element(dp, dp + MAXW) << '\n';
+}
+
+int main() {
+    fast_input;
+#ifdef __APPLE__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+}
