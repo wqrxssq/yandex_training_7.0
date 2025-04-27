a92e495def324ebb4f31403e3e62edab83670332
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-04-28 00:39:39 +0300

    E

---
+++ tasks/contest_3/E/main.cpp
@@ -0,0 +1,61 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#include <string>
+#include <stack>
+#include <cassert>
+#include <iomanip>
+#include <random>
+#include <chrono>
+#include <cstring>
+#include <bitset>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector <int>;
+using vvi = vector <vi>;
+using vll = vector <ll>;
+using pii = pair <int, int>;
+using pll = pair <ll, ll>;
+using vpii = vector <pii>;
+const double eps = 0.0001;
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD = 1e9 + 7;
+
+#define all(_x) _x.begin(), _x.end()
+#define rall(_x) _x.rbegin(), _x.rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(_x) (int)_x.size()
+#define display(_x) for (auto el : _x) cout << el << ' '; cout << '\n'
+#define cin_arr(_x) for (auto &el : _x) cin >> el;
+#define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
+#define setpr cout << setprecision(6) << fixed
+
+void solve() {
+    int x1, y1;
+    cin >> x1 >> y1;
+    cout << (x1 ^ y1) << '\n';
+    int x2, c;
+    cin >> x2 >> c;
+    cout << (x2 ^ c) << '\n';
+}
+
+int main() {
+    fast_input;
+#ifdef __APPLE__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+}
