ba9330f1fa3ae5756306445782f5983ccb0313da
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-04-28 20:38:07 +0300

    J

--- tasks/contest_3/J/main.cpp
+++ tasks/contest_3/J/main.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
 #include <algorithm>
+#include <array>
 #include <bitset>
 #include <climits>
 #include <vector>
@@ -42,162 +43,157 @@ const int MOD = 1e9 + 7;
 #define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
 #define setpr cout << setprecision(6) << fixed
 
+const int alphabet_size = 26;
+const int MAX_MASK = 255;
+
 void solve() {
-    string mode;
-    cin >> mode;
-    if (mode == "pack") {
+    string op;
+    cin >> op;
+    if (op == "pack") {
         string s;
         cin >> s;
         int n = sz(s);
-        // Build dictionary as a trie (prefix -> code)
-        struct Node { int next[26]; Node() { for(int i=0;i<26;i++) next[i] = -1; } };
-        vector<Node> dict;
-        dict.reserve(n * 2);
-        // Initialize dictionary with 'a'..'z'
-        for (int i = 0; i < 26; ++i) {
-            dict.push_back(Node());
+
+        int initial_bits = 0;
+        while ((1 << initial_bits) < alphabet_size) {
+            initial_bits++;
         }
-        int dict_size = 26;
-        int code_width = 0;
-        while ((1 << code_width) < dict_size) code_width++;
-        if (code_width == 0) code_width = 1;
-        int next_limit = 1 << code_width;
-        vector<unsigned char> out_bytes;
-        out_bytes.reserve(n);
-        unsigned int bit_buffer = 0;
+        int code_width = initial_bits;
+        int next_code = alphabet_size;
+
+        vector<array<int, alphabet_size>> dict_map;
+        dict_map.reserve(n);
+        for (int i = 0; i < alphabet_size; i++) {
+            array<int,alphabet_size> arr;
+            arr.fill(-1);
+            dict_map.pb(arr);
+        }
+
+        uint64_t bit_buffer = 0;
         int bit_count = 0;
+        vector<uint8_t> out_bytes;
+        out_bytes.reserve(n / 2);
 
         int prefix = s[0] - 'a';
-        for (int i = 1; i < n; ++i) {
+        for (int i = 1; i < n; i++) {
             int c = s[i] - 'a';
-            if (dict[prefix].next[c] != -1) {
-                prefix = dict[prefix].next[c];
+            int idx = dict_map[prefix][c];
+            if (idx != -1) {
+                prefix = idx;
             } else {
-                // Output the code for prefix
-                bit_buffer |= ((unsigned int)prefix << bit_count);
+                bit_buffer |= (uint64_t)prefix << bit_count;
                 bit_count += code_width;
                 while (bit_count >= 8) {
-                    out_bytes.push_back((unsigned char)(bit_buffer & 0xFF));
+                    out_bytes.pb(bit_buffer & MAX_MASK);
                     bit_buffer >>= 8;
                     bit_count -= 8;
                 }
-                // Add new string prefix+c to dictionary
-                dict[prefix].next[c] = dict_size;
-                dict.push_back(Node());
-                dict_size++;
-                if (dict_size == next_limit) {
+                dict_map[prefix][c] = next_code;
+                array<int,alphabet_size> new_arr;
+                new_arr.fill(-1);
+                dict_map.pb(new_arr);
+                next_code++;
+                if (next_code == (1 << code_width)) {
                     code_width++;
-                    next_limit <<= 1;
                 }
                 prefix = c;
             }
         }
-        // Output the code for the last prefix
-        bit_buffer |= ((unsigned int)prefix << bit_count);
+        bit_buffer |= (uint64_t)prefix << bit_count;
         bit_count += code_width;
         while (bit_count >= 8) {
-            out_bytes.push_back((unsigned char)(bit_buffer & 0xFF));
+            out_bytes.pb(bit_buffer & MAX_MASK);
             bit_buffer >>= 8;
             bit_count -= 8;
         }
-        // Write any remaining bits as a byte (pad with zeros)
         if (bit_count > 0) {
-            out_bytes.push_back((unsigned char)(bit_buffer & 0xFF));
+            out_bytes.pb(bit_buffer & MAX_MASK);
         }
-        // Print result: number of bytes and each byte (0..255)
-        cout << out_bytes.size();
-        for (unsigned char b : out_bytes) {
-            cout << " " << (int)b;
+
+        int N = sz(out_bytes);
+        cout << N << '\n';
+        for (int i = 0; i < N; i++) {
+            cout << (int)out_bytes[i] << (i+1 < N ? ' ' : '\n');
         }
-        cout << endl;
-        cout.flush();
-    } else if (mode == "unpack") {
-        int m;
-        cin >> m;
-        vector<unsigned char> in_bytes(m);
-        for (int i = 0; i < m; ++i) {
+        cout << flush;
+
+    } else if (op == "unpack") {
+        int N;
+        cin >> N;
+        vector<uint8_t> data(N);
+        for (int i = 0; i < N; i++) {
             int x;
             cin >> x;
-            in_bytes[i] = (unsigned char)x;
+            data[i] = (uint8_t)x;
         }
-        // Prepare dictionary for decompression
-        vector<int> dict_prefix;
-        vector<char> dict_char;
-        dict_prefix.reserve(m * 2);
-        dict_char.reserve(m * 2);
-        for (int i = 0; i < 26; ++i) {
-            dict_prefix.push_back(-1);
-            dict_char.push_back((char)('a' + i));
+
+        int initial_bits = 0;
+        while ((1 << initial_bits) < alphabet_size) {
+            initial_bits++;
         }
-        int dict_size = 26;
-        int code_width = 0;
-        while ((1 << code_width) < dict_size) code_width++;
-        if (code_width == 0) code_width = 1;
-        int next_limit = 1 << code_width;
-        unsigned int bit_buffer = 0;
-        int bit_count = 0;
-        int byte_index = 0;
-        auto readCode = [&](int width) {
-            while (bit_count < width && byte_index < (int)in_bytes.size()) {
-                bit_buffer |= ((unsigned int)in_bytes[byte_index] << bit_count);
+        int code_width = initial_bits;
+        int next_code = alphabet_size;
+
+        vector<pair<int,char>> dict_rev;
+        dict_rev.reserve(N);
+        for (int i = 0; i < alphabet_size; i++) {
+            dict_rev.emplace_back(-1, char('a' + i));
+        }
+
+        uint64_t bit_buffer = 0;
+        int bit_count = 0, pos = 0;
+        auto read_code = [&]() -> int {
+            while (bit_count < code_width && pos < N) {
+                bit_buffer |= (uint64_t)data[pos++] << bit_count;
                 bit_count += 8;
-                byte_index++;
             }
-            if (bit_count < width) return -1;
-            unsigned int code = bit_buffer & ((1u << width) - 1);
-            bit_buffer >>= width;
-            bit_count -= width;
-            return (int)code;
+            if (bit_count < code_width) return -1;
+            int code = bit_buffer & ((1 << code_width) - 1);
+            bit_buffer >>= code_width;
+            bit_count -= code_width;
+            return code;
         };
-        // Read first code
-        int prev_code = readCode(code_width);
-        if (prev_code >= 0 && prev_code < 26) {
-            cout << (char)('a' + prev_code);
-        }
-        // Process remaining codes
-        while (true) {
-            if (dict_size == next_limit) {
-                code_width++;
-                next_limit <<= 1;
+
+        auto reconstruct = [&](int code) {
+            string tmp;
+            while (code != -1) {
+                tmp.push_back(dict_rev[code].second);
+                code = dict_rev[code].first;
             }
-            int curr_code = readCode(code_width);
-            if (curr_code < 0) break;
+            reverse(tmp.begin(), tmp.end());
+            return tmp;
+        };
+
+        int first_code = read_code();
+        if (first_code < 0) return;
+        string w = reconstruct(first_code);
+        string result;
+        result.reserve(2 * N);
+        result += w;
+        int prev_code = first_code;
+
+        while (true) {
+            int code = read_code();
+            if (code < 0) break;
             string entry;
-            if (curr_code < dict_size) {
-                int code = curr_code;
-                string temp;
-                while (code >= 26) {
-                    temp.push_back(dict_char[code]);
-                    code = dict_prefix[code];
-                }
-                temp.push_back((char)('a' + code));
-                reverse(temp.begin(), temp.end());
-                entry = temp;
-            } else if (curr_code == dict_size) {
-                // Special case: code equals dict_size (KwKwK case)
-                int code = prev_code;
-                string temp;
-                while (code >= 26) {
-                    temp.push_back(dict_char[code]);
-                    code = dict_prefix[code];
-                }
-                temp.push_back((char)('a' + code));
-                reverse(temp.begin(), temp.end());
-                char firstChar = temp.back();
-                entry = temp;
-                entry.push_back(firstChar);
+            if (code < next_code) {
+                entry = reconstruct(code);
+            } else if (code == next_code) {
+                entry = w + w[0];
             } else {
                 break;
             }
-            cout << entry;
-            char firstCharEntry = entry[0];
-            dict_prefix.push_back(prev_code);
-            dict_char.push_back(firstCharEntry);
-            dict_size++;
-            prev_code = curr_code;
+            result += entry;
+            dict_rev.emplace_back(prev_code, entry[0]);
+            next_code++;
+            if (next_code == ((1 << code_width) - 1)) {
+                code_width++;
+            }
+            prev_code = code;
+            w = entry;
         }
-        cout << endl;
-        cout.flush();
+
+        cout << result << '\n' << flush;
     }
 }
 
