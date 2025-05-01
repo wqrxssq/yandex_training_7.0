d807f1345077d8482aec98de40681a158eeaadfb
Nasedkin Dmitry <jamesnasedkin@gmail.com>
2025-05-01 17:58:44 +0300

    F

---
+++ tasks/contest_4/F/main.cpp
@@ -0,0 +1,89 @@
+#include <iostream>
+#include <algorithm>
+#include <array>
+#include <bitset>
+#include <climits>
+#include <vector>
+#include <math.h>
+#include <set>
+#include <unordered_set>
+#include <map>
+#include <unordered_map>
+#include <deque>
+#include <queue>
+#include <string>
+#include <stack>
+#include <cassert>
+#include <iomanip>
+#include <random>
+#include <chrono>
+#include <cstring>
+#include <list>
+using namespace std;
+
+using ll = long long;
+using ull = unsigned long long;
+using vi = vector <int>;
+using vvi = vector <vi>;
+using vll = vector <ll>;
+using pii = pair <int, int>;
+using vpii = vector <pii>;
+const double eps = 0.0001;
+const int INF = 1e9;
+const ll INFLL = 1e18;
+const int MOD = 1e9 + 7;
+
+#define all(_x) _x.begin(), _x.end()
+#define rall(_x) _x.rbegin(), _x.rend()
+#define pb push_back
+#define ff first
+#define ss second
+#define sz(_x) (int)_x.size()
+#define display(_x) for (auto el : _x) cout << el << ' '; cout << '\n'
+#define cin_arr(_x) for (auto &el : _x) cin >> el;
+#define fast_input ios_base::sync_with_stdio(0); cin.tie(0)
+#define setpr cout << setprecision(6) << fixed
+
+const int MAXN = 100000;
+int n;
+vi g[MAXN];
+int used[MAXN];
+
+void dfs(int v) {
+    used[v] = 1;
+    for (int u : g[v]) {
+        if (!used[u]) {
+            dfs(u);
+        }
+    }
+}
+
+void solve() {
+    cin >> n;
+    for (int v = 0; v < n; v++) {
+        int u;
+        cin >> u;
+        u--;
+        g[v].pb(u);
+        g[u].pb(v);
+    }
+
+    int res = 0;
+    for (int v = 0; v < n; v++) {
+        if (!used[v]) {
+            res++;
+            dfs(v);
+        }
+    }
+    cout << res << '\n';
+}
+
+int main() {
+    fast_input;
+#ifdef __APPLE__
+    freopen("in.txt", "r", stdin);
+    freopen("out.txt", "w", stdout);
+#endif
+    solve();
+    return 0;
+}
